02d8952d2f7d7eaafe1087f5d99e591c8d509dba
root <poprazvan09@gmail.com>
2026-02-24 23:58:58 +0200

    impartire utils in mai multe fisiere

---
+++ utils/__init__.py
@@ -0,0 +1,19 @@
+# utils/__init__.py
+
+# Importăm funcțiile din modulele interne pentru a le face accesibile direct din 'utils'
+from .ui_styles import (
+    set_galaxy_background, 
+    set_sidebar_style, 
+    apply_pro_plotting_style
+)
+
+from .data_fetchers import (
+    get_toi_catalog, 
+    search_toi_catalog, 
+    fetch_star_data, 
+    get_star_parameters, 
+    fetch_catalog_targets, 
+    fetch_untested_targets
+)
+
+from .analysis_engine import process_selected_data
